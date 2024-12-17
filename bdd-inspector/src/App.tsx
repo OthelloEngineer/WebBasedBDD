acfee2691f6156f0274acc9eda41b7026dab4983
OthelloEngineer <jolee18@student.sdu.dk>
2024-12-17 13:56:43 +0100

    added changes

--- bdd-inspector/src/App.tsx
+++ bdd-inspector/src/App.tsx
@@ -1,20 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import FileInspector from './component/FileInspector';
 import DependencyManager from './component/DependencyManager';
-
+import './App.css';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-      <FileInspector />
-      <div className="flex-container">
-        <DependencyManager />
+    <Router>
+      <div className="App">
+<header className="App-header">
+  <h1>My Application</h1>
+  <nav>
+    <ul className="nav-list">
+      <li><Link to="/">Home</Link></li>
+      <li><Link to="/file-inspector">File Inspector</Link></li>
+      <li><Link to="/dependency-manager">Dependency Manager</Link></li>
+    </ul>
+  </nav>
+</header>
+
+        
+        <div className="content-container">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/file-inspector" element={<FileInspector />} />
+            <Route path="/dependency-manager" element={<DependencyManager />} />
+          </Routes>
+        </div>
       </div>
-    </div>
+    </Router>
   );
+}
+
+function Home() {
+  return <h2>Welcome to My Application</h2>;
 }
 
 export default App;
